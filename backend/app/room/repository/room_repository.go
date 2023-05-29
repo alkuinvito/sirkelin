2bc233f93305aad56816011cf857f42a151460c4
alkuinvito <alkuinvito@tutanota.com>
2023-05-29 23:26:04 +0700

    refactor: move db to parameter

--- backend/app/room/repository/room_repository.go
+++ backend/app/room/repository/room_repository.go
@@ -1,53 +1,52 @@
 package repository
 
 import (
-	"gorm.io/gorm"
-	"sirkelin/backend/initializers"
 	"sirkelin/backend/models"
+
+	"gorm.io/gorm"
 )
 
 type RoomRepository struct {
-	db *gorm.DB
 }
 
 type IRoomRepository interface {
-	Count(room *models.Room, uid string) (int, error)
-	FindByID(uid string) ([]models.Room, error)
-	Insert(room *models.Room) error
-	Peek(room *models.Room) error
+	Count(db *gorm.DB, room *models.Room, uid string) (int, error)
+	Create(db *gorm.DB, room *models.Room) error
+	GetByID(db *gorm.DB, uid string) ([]models.Room, error)
+	Peek(db *gorm.DB, room *models.Room) error
 }
 
 func NewRoomRepository() *RoomRepository {
-	return &RoomRepository{db: initializers.DB}
+	return &RoomRepository{}
 }
 
-func (repo *RoomRepository) Count(room *models.Room, uid string) (int, error) {
+func (repo *RoomRepository) Count(db *gorm.DB, room *models.Room, uid string) (int, error) {
 	var rows int64
-	err := repo.db.Table("user_rooms").Where("room_id = ? AND user_id = ?", room.ID, uid).Count(&rows).Error
+	err := db.Table("user_rooms").Where("room_id = ? AND user_id = ?", room.ID, uid).Count(&rows).Error
 	if err != nil {
 		return -1, err
 	}
 	return int(rows), nil
 }
 
-func (repo *RoomRepository) FindByID(uid string) ([]models.Room, error) {
-	var result []models.Room
-	err := repo.db.Table("user_rooms").Where("user_rooms.user_id = ?", uid).Joins("join rooms on rooms.id = user_rooms.room_id").Where("is_private = ?", false).Scan(&result).Error
-	if err != nil {
-		return []models.Room{}, err
-	}
-	return result, nil
-}
-
-func (repo *RoomRepository) Insert(room *models.Room) error {
-	err := repo.db.Create(room).Error
+func (repo *RoomRepository) Create(db *gorm.DB, room *models.Room) error {
+	err := db.Create(room).Error
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (repo *RoomRepository) Peek(room *models.Room) error {
+func (repo *RoomRepository) GetByID(db *gorm.DB, uid string) ([]models.Room, error) {
+	var result []models.Room
+	err := db.Table("user_rooms").Where("user_rooms.user_id = ?", uid).Joins("join rooms on rooms.id = user_rooms.room_id").Where("is_private = ?", false).Scan(&result).Error
+	if err != nil {
+		return []models.Room{}, err
+	}
+	return result, nil
+}
+
+func (repo *RoomRepository) Peek(db *gorm.DB, room *models.Room) error {
 	var result models.Room
-	return repo.db.Table("messages").Where("room_id = ?", room.ID).Find(&(result.Messages)).Error
+	return db.Table("messages").Where("room_id = ?", room.ID).Find(&(result.Messages)).Error
 }
