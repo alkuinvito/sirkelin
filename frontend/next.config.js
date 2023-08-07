cb3fa101a26222537f9a72dbdd499dcfd89507bc
alkuinvito <alkuinvito@tutanota.com>
2023-08-07 22:18:00 +0700

    feat(client): rebuild frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  output: "standalone",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-};
+const nextConfig = {}
+
+module.exports = nextConfig
